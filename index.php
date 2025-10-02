294fc8d65def72ffa9fbfec9ecca413e5b3f442f
TheScarletWarlock645 <angeloreid24@gmail.com>
2025-10-02 19:53:26 -0400

    Added first few files

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include('head.html'); ?>
+        <title>Free Guitar Lessons</title>
+    </head>
+    <body>
+        <h1>Free Guitar Lessons</h1>
+        <div class="content">
+            <h2>Welcome to free guitar lessons! Look below to get started with modules.</h2>
+            <div class="module">
+
+            </div>
+        </div>
+        <script>
+
+        </script>
+    </body>
+</html>
