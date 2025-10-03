ff3bdd2827dc8a7916fac1c186892c7c9e42606e
TheScarletWarlock645 <angeloreid24@gmail.com>
2025-10-02 20:10:18 -0400

    Reorganized files and added mod 1 text

--- index.php
+++ index.php
@@ -9,11 +9,16 @@
         <div class="content">
             <h2>Welcome to free guitar lessons! Look below to get started with modules.</h2>
             <div class="module">
-
+                <img src="#" alt="mod_1">
+                <h3>Module 1</h3>
+                <p>In this first module we go over the first basic steps for begginer guitarists.</p>
+                <ul>
+                    <li>Capos</li>
+                    <li>Parts of a guitar</li>
+                    <li>Standard tuning</li>
+                    <li>Guitar picks</li>
+                </ul>
             </div>
         </div>
-        <script>
-
-        </script>
     </body>
 </html>
