26474fbe8332960ab96ead39716e3f7104a97de9
TheScarletWarlock645 <angeloreid24@gmail.com>
2025-10-05 17:36:48 -0400

    Renamed navbar, added images, and fixed favicon

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
-<!DOCTYPE html>
+<!DOCTYPE hS tml>
 <html>
     <head>
-        <?php include('head.html'); ?>
+        <?php include('./recources/head.html'); ?>
         <title>Free Guitar Lessons</title>
     </head>
     <body>
+        <?php include('./recources/nav.php'); ?>
         <h1>Free Guitar Lessons</h1>
         <div class="content">
             <h2>Welcome to free guitar lessons! Look below to get started with modules.</h2>
             <div class="module">
-                <img src="#" alt="mod_1">
+                <img src="./images/lesson1fgl.jpeg" alt="mod_1">
                 <h3>Module 1</h3>
                 <p>In this first module we go over the first basic steps for begginer guitarists.</p>
                 <ul>
@@ -20,7 +21,7 @@
                 </ul>
             </div>
             <div class="module">
-                <img src="#" alt="mod_2">
+                <img src="./images/lesson2fgl.jpeg" alt="mod_2">
                 <h3>Module 2</h3>
                 <p>The second module covers first chords and a basic song.</p>
                 <ul>
---
+++ recources/nav.php
@@ -0,0 +1,5 @@
+<ul>
+    <li><a href="./modules.php">Modules</a></li>
+    <li><a href="./contact.php">Contact</a></li>
+    <li><a href="./login.php">Log in</a></li>
+</ul>
